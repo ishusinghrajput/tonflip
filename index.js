3569eac2d8efd2e681a542af3e9c1e8fb9551b07
Ishu Singh Rajput <59223389+ishusinghrajput@users.noreply.github.com>
2024-07-03 01:29:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 
+
+
+
 let flipbtn = document.getElementById("flipbtn");
 
 let heads = 0;
 let tails = 0;
+let optionselected = 1;
+let amountselected = 0.25;
+
+
+async function bettransaction(){
+    const transaction = {
+  messages: [
+    {
+        address: "UQDx4nLudgg-yWp6tWcG0EGCM-6dA8DBoLDuicnUzNNvhyzp", // destination address
+        amount: amountselected * 1000000000 //Toncoin in nanotons
+    }
+   ]
+  }
+
+  const result = await tonConnectUI.sendTransaction(transaction)
+  alert("Transaction complete");
+  flipbtnclick();
+}
 
 
 function flipbtnclick(){
@@ -34,7 +55,12 @@ function flipbtnclick(){
     }
 
     function updateStats(){
-
+        if(i == optionselected){
+            alert("You Won! DOUBLED")
+        }
+        else{
+            alert("You Lost! Rugged")
+        }
     }
     
     function disableButton(){
@@ -44,4 +70,123 @@ function flipbtnclick(){
             flipbtn.disabled = false;
         },3000)
     }
+}
+
+
+function headsbtnclick(){
+    tailsbtn = document.getElementById("tailsbtn");
+    tailsbtn.style.border = "none" ;
+    headsbtn = document.getElementById("headsbtn");
+    headsbtn.style.border = "3px solid black" ;
+    optionselected = 1;
+}
+
+function tailsbtnclick(){
+    headsbtn = document.getElementById("headsbtn");
+    headsbtn.style.border = "none" ;
+    tailsbtn = document.getElementById("tailsbtn");
+    tailsbtn.style.border = "3px solid black" ;
+    optionselected = 0;
+}
+
+function amount1click(){
+    amount1 = document.getElementById("amount1");
+    amount2 = document.getElementById("amount2");
+    amount3 = document.getElementById("amount3");
+    amount4 = document.getElementById("amount4");
+    amount5 = document.getElementById("amount5");
+    amount6 = document.getElementById("amount6");
+    amount1.style.border = "none" ;
+    amount2.style.border = "none" ;
+    amount3.style.border = "none" ;
+    amount4.style.border = "none" ;
+    amount5.style.border = "none" ;
+    amount6.style.border = "none" ;
+    amount1.style.border = "3px solid black" ;
+    amountselected = 0.25;
+}
+
+function amount2click(){
+    amount1 = document.getElementById("amount1");
+    amount2 = document.getElementById("amount2");
+    amount3 = document.getElementById("amount3");
+    amount4 = document.getElementById("amount4");
+    amount5 = document.getElementById("amount5");
+    amount6 = document.getElementById("amount6");
+    amount1.style.border = "none" ;
+    amount2.style.border = "none" ;
+    amount3.style.border = "none" ;
+    amount4.style.border = "none" ;
+    amount5.style.border = "none" ;
+    amount6.style.border = "none" ;
+    amount2.style.border = "3px solid black" ;
+    amountselected = 0.50;
+}
+
+function amount3click(){
+    amount1 = document.getElementById("amount1");
+    amount2 = document.getElementById("amount2");
+    amount3 = document.getElementById("amount3");
+    amount4 = document.getElementById("amount4");
+    amount5 = document.getElementById("amount5");
+    amount6 = document.getElementById("amount6");
+    amount1.style.border = "none" ;
+    amount2.style.border = "none" ;
+    amount3.style.border = "none" ;
+    amount4.style.border = "none" ;
+    amount5.style.border = "none" ;
+    amount6.style.border = "none" ;
+    amount3.style.border = "3px solid black" ;
+    amountselected = 1;
+}
+
+function amount4click(){
+    amount1 = document.getElementById("amount1");
+    amount2 = document.getElementById("amount2");
+    amount3 = document.getElementById("amount3");
+    amount4 = document.getElementById("amount4");
+    amount5 = document.getElementById("amount5");
+    amount6 = document.getElementById("amount6");
+    amount1.style.border = "none" ;
+    amount2.style.border = "none" ;
+    amount3.style.border = "none" ;
+    amount4.style.border = "none" ;
+    amount5.style.border = "none" ;
+    amount6.style.border = "none" ;
+    amount4.style.border = "3px solid black" ;
+    amountselected = 2;
+}
+
+function amount5click(){
+    amount1 = document.getElementById("amount1");
+    amount2 = document.getElementById("amount2");
+    amount3 = document.getElementById("amount3");
+    amount4 = document.getElementById("amount4");
+    amount5 = document.getElementById("amount5");
+    amount6 = document.getElementById("amount6");
+    amount1.style.border = "none" ;
+    amount2.style.border = "none" ;
+    amount3.style.border = "none" ;
+    amount4.style.border = "none" ;
+    amount5.style.border = "none" ;
+    amount6.style.border = "none" ;
+    amount5.style.border = "3px solid black" ;
+    amountselected = 5;
+}
+
+function amount6click(){
+    amount1 = document.getElementById("amount1");
+    amount2 = document.getElementById("amount2");
+    amount3 = document.getElementById("amount3");
+    amount4 = document.getElementById("amount4");
+    amount5 = document.getElementById("amount5");
+    amount6 = document.getElementById("amount6");
+    amount1.style.border = "none" ;
+    amount2.style.border = "none" ;
+    amount3.style.border = "none" ;
+    amount4.style.border = "none" ;
+    amount5.style.border = "none" ;
+    amount6.style.border = "none" ;
+    amount6.style.border = "3px solid black" ;
+    amountselected = 10;
 }
